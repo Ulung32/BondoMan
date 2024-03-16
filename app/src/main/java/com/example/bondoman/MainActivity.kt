0397f9ea5727e3b58c015313131f09f253ba82d7
Ulung32 <ulung1208@gmail.com>
2024-03-17 01:28:13 +0700

    add recycler view for transaction

--- app/src/main/java/com/example/bondoman/MainActivity.kt
+++ app/src/main/java/com/example/bondoman/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.bondoman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bondoman.Room.TransactionEntity
+import com.example.bondoman.ui.Transaction.TransactionAdapter
+import java.time.LocalDateTime
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        Thread.sleep(1000)
+        installSplashScreen()
+        setContentView(R.layout.transaction)
+
+        val rv: RecyclerView = findViewById(R.id.transaction_rv)
+        rv.layoutManager = LinearLayoutManager(this)
+
+//        val data = arrayOf(
+//            TransactionEntity(1, "title1", "pemasukan", 10000, 1, 1, LocalDateTime.now() ),
+//            TransactionEntity(2, "title2", "pemasukan", 10000, 1, 1, LocalDateTime.now() ),
+//            TransactionEntity(3, "title3", "pemasukan", 10000, 1, 1, LocalDateTime.now() )
+//        )
+        val data = listOf(
+            TransactionEntity(1, "title1", "pemasukan", 10000, 1, 1, LocalDateTime.now()),
+            TransactionEntity(2, "title2", "pemasukan", 10000, 1, 1, LocalDateTime.now()),
+            TransactionEntity(3, "title3", "pemasukan", 10000, 1, 1, LocalDateTime.now())
+        )
+
+
+        rv.adapter = TransactionAdapter(data)
     }
 }
---
+++ app/src/main/java/com/example/bondoman/ui/Transaction/TransactionAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.bondoman.ui.Transaction
+
+import android.nfc.Tag
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bondoman.Room.TransactionEntity
+import com.example.bondoman.databinding.TransactionItemBinding
+
+
+class TransactionAdapter (private val listData: List<TransactionEntity>) : RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder>() {
+    class TransactionViewHolder private constructor(val binding: TransactionItemBinding) : RecyclerView.ViewHolder(binding.root){
+        companion object{
+            fun from (parent: ViewGroup): TransactionViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = TransactionItemBinding.inflate(layoutInflater, parent, false)
+                return TransactionViewHolder(binding)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return listData.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TransactionViewHolder {
+        return TransactionViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: TransactionViewHolder, position: Int) {
+        holder.binding.titleTv.text = listData[position].title
+        holder.binding.categoryTv.text = listData[position].category
+        holder.binding.priceTv.text = "Rp. ".plus(listData[position].nominal)
+        holder.binding.locationTv.text = "TO DO!!!"
+        holder.binding.dateTv.text = listData[position].date.toString()
+    }
+
+}
