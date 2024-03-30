eb14e513755abadde64d5d15dea6ba61d4a2b712
Ulung32 <ulung1208@gmail.com>
2024-03-30 15:48:29 +0700

    Feat transaction layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+
 }
 
 android {
@@ -51,13 +53,14 @@ dependencies {
     implementation("androidx.annotation:annotation:1.6.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.7")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
     implementation("androidx.room:room-runtime:2.6.1")
     implementation("androidx.room:room-ktx:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
 
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("javax.inject:javax.inject:1")
@@ -67,4 +70,8 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("com.google.android.material:material:1.2.0-alpha06")
+
+    kapt("androidx.room:room-compiler:2.6.1")
+
+    annotationProcessor ("androidx.room:room-compiler:2.6.1")
 }
