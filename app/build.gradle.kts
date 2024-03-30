3a427e2ec9fd548ab3a143d0bf8c14e522b7f9e5
Mutawally Nawwar <13521065@std.stei.itb.ac.id>
2024-03-30 20:42:03 +0700

    setup chart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,4 +67,5 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
     implementation("com.google.android.material:material:1.2.0-alpha06")
+    implementation("com.diogobernardino:williamchart:3.10.1")
 }
--- app/src/main/java/com/example/bondoman/ui/ChartFragment.kt
+++ app/src/main/java/com/example/bondoman/ui/ChartFragment.kt
@@ -1,20 +1,43 @@
 package com.example.bondoman.ui
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.bondoman.R
+import com.example.bondoman.databinding.FragmentChartBinding
 
 class ChartFragment : Fragment() {
+
+    private lateinit var binding: FragmentChartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        binding = FragmentChartBinding.inflate(inflater, container, false)
+        binding.apply {
+            donutChart.donutColors = intArrayOf(
+                Color.parseColor("#FFFFFF"),
+                Color.parseColor("#DDDDDD"),
+                Color.parseColor("#111111")
+            )
+            donutChart.animation.duration = animationDuration
+            donutChart.animate(donutSet)
+        }
+        return binding.root
     }
+    companion object {
 
+        private val donutSet = listOf(
+            20f,
+            50f,
+            30f
+        )
+
+        private const val animationDuration = 1000L
+    }
 }
